d8f1db6bfd79fb3346ad4a692739006a39569ac5
Luis Huertas <77518276+lc-huertas@users.noreply.github.com>
2021-05-06 18:20:43 -0400

    assignment

---
+++ 01-java-fundamentals/01-First Java Program/LCH_01_FirstProgram.java
@@ -0,0 +1,7 @@
+public class LCH_01_FirstProgram {
+    public static void main(String[] args){
+        System.out.println("My name is Luis Huertas");
+        System.out.println("I am 28 years old");
+        System.out.println("My hometown is Bogota, Colombia");
+    }
+}
